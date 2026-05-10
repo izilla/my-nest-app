e13dfef2e32ef156fafe4585996eaee8ff1b9d0e
izilla <isaiahgriego@gmail.com>
2026-05-10 11:03:45 -0500

    fix build

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { SecurityModule } from '../security/security.module';
+import { SecurityService } from '../security/security.service';
 import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [PrismaModule, SecurityModule],
+  imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService, UsersService, SecurityService],
 })
 export class AuthModule {}
