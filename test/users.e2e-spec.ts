07883f802dc230eb52a1322168f81947d45e959f
izilla <isaiahgriego@gmail.com>
2026-05-10 11:07:26 -0500

    fix tests

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -5,6 +5,7 @@ import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { SecurityService } from '../src/security/security.service';
 
 describe('UsersController (e2e)', () => {
   let app: INestApplication<App>;
@@ -13,6 +14,7 @@ describe('UsersController (e2e)', () => {
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [SecurityService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
